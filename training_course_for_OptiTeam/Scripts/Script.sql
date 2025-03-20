cdc9dcdea89f4906ddbb9899a40384f40d4ccd11
VIWARR <dm.pavlyuchenko@gmail.com>
2025-03-20 12:15:33 +0300

    backup

--- training_course_for_OptiTeam/Scripts/Script.sql
+++ training_course_for_OptiTeam/Scripts/Script.sql
@@ -1,1 +1,177 @@
-SELECT 'hello'
+create table payments (
+	id serial primary key,
+	amount numeric(10, 2),
+	date_start date,
+	date_end date,
+	customer_group char(1),
+	customer_status text
+);
+
+
+insert into payments(
+	amount,
+	date_start,
+	date_end,
+	customer_group,
+	customer_status
+)
+select
+	round((random() * 125008)::numeric, 2),
+	(current_date - (random() * 365)::int),
+	(current_date + (random() * 365)::int),
+	(array['A', 'B', 'C', 'D'])[floor(random() * 4) + 1],
+	case when random() < 0.8 then 'Активный' else 'Неактивный' end
+from generate_series(1, 30);
+
+select * from payments;
+
+with split_pay_by_day as(
+	select
+		p.id,
+		gs::date as payment_date,
+		p.amount / nullif((p.date_end - p.date_start), 0) as daily_amount,
+		p.customer_group,
+		p.customer_status
+	from payments p
+	cross join generate_series(p.date_start, p.date_end, '1 day'::interval) as gs
+)
+
+
+select * from split_pay_by_day order by id, payment_date;
+
+
+
+with split_pay_by_day as(
+	select
+		p.id,
+		gs::date as payment_date,
+		p.amount,
+		nullif((p.date_end - p.date_start), 0) as count_day,
+		p.customer_group,
+		p.customer_status
+	from payments p
+	cross join generate_series(p.date_start, p.date_end, '1 day'::interval) as gs
+)
+
+select
+	id,
+	payment_date,
+	amount,
+	count_day,
+	amount / count_day as daily_amount, -- Находим сумму платежа, распределенную равномерно по дням
+	customer_group,
+	customer_status
+from 
+	split_pay_by_day 
+order by 
+	id, payment_date;
+
+
+
+
+with split_pay_by_month as(
+	select
+		p.id,
+		gs::date as month_start,
+		p.customer_group,
+		p.customer_status,
+		p.amount / nullif(extract(month from age(p.date_end, p.date_start)), 0) as monthly_amount
+	from payments p 
+	cross join generate_series(
+		date_trunc('month', p.date_start),
+		date_trunc('month', p.date_end),
+		'1 month'::interval
+	) as gs
+)
+
+
+with split_pay_by_month as(
+	select
+		p.id,
+		p.amount,
+		gs::date as month_start,
+		p.date_end,
+		extract(month from age(p.date_end, p.date_start)) as count_months,
+		p.customer_group,
+		p.customer_status
+	from payments p 
+	cross join generate_series(
+		date_trunc('month', p.date_start),
+		date_trunc('month', p.date_end),
+		'1 month'::interval
+	) as gs
+)
+
+
+select
+	id,
+	amount,
+	count_months,
+	amount / nullif(count_months, 0) as monthly_amount,
+	customer_group,
+	customer_status
+from
+	split_pay_by_month
+order by id, month_start;
+
+
+with split_pay_by_week as (
+    select
+        p.id,
+        gs::date as week_start,  -- Начало недели в интервале платежа
+        p.date_end,
+        ceil((p.date_end - p.date_start + 1) / 7.0) as count_weeks,  -- Количество недель в диапазоне платежа
+        p.amount,
+        p.customer_group,
+        p.customer_status
+    from payments p 
+    cross join generate_series(
+        date_trunc('week', p.date_start),  -- Начало недели для диапазона платежа
+        date_trunc('week', p.date_end),  -- Конец недели для диапазона платежа
+        '1 week'::interval  -- Шаг генерации - 1 неделя
+    ) as gs
+)
+
+
+select 
+	id,
+	min(amount) as amount,
+	min(count_weeks) as count_weeks
+from 
+	split_pay_by_week 
+group by id;
+select
+    id,
+    amount,
+    count_weeks,
+    amount / nullif(count_weeks, 0) as weekly_amount,  -- Сумма платежа, распределенная по неделям
+    customer_group,
+    customer_status
+from split_pay_by_week
+order by id, week_start;
+
+select 
+	id,
+	min(amount) as amount,
+	min(count_weeks) as count_weeks
+from 
+	split_pay_by_week 
+group by id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- training_course_for_OptiTeam/Scripts/window_functions.sql
+++ training_course_for_OptiTeam/Scripts/window_functions.sql
@@ -1,9 +1,5 @@
 select * from auto;
----------------------------------------------------
-
-create table auto_backup as select * from auto;
----------------------------------------------------
-
+				 
 with ranked_autos as (
     select
         row_number() over (partition by brand order by ctid) AS row_num,
@@ -11,12 +7,150 @@ with ranked_autos as (
     from
         auto
 )
-SELECT *
-FROM ranked_autos;
----------------------------------------------------
+delete from auto
+where ctid in (
+    select ctid
+    from ranked_autos
+    where row_num > 7
+);
+ALTER TABLE auto
+ADD COLUMN id SERIAL PRIMARY KEY;
+-------------------практические примеры -----------------------
+--- рассчитаем накопительную сумму цен для каждого model, но только для машин с ценой выше 4000
+select 
+	brand,
+	model,
+	price,
+	sum(case when price > 4000 then price else 0 end) over (
+		partition by brand, model
+		order by model
+		rows between unbounded preceding and current row --- окно от первой строки в группе до текущей
+	) as comulative_price_model_more_than_4000
+from auto
+order by brand, model;
 
-create table auto_backup as select * from auto;
----------------------------------------------------
+--- расчет разницы между текущим значением и средним
+with auto_avg as(
+	select
+		brand,
+		model,
+		price,
+		avg(price) over (partition by model)::int as avg_price
+	from auto
+)
+select
+	*,
+	price - avg_price as price_diff
+from auto_avg;
+
+--- поиск разрыва системного id
+with ctid_auto as(
+	select
+		ctid,
+		brand,
+		model,
+		lag(ctid) over (partition by brand) as prev_ctid,
+		lag(ctid) over (partition by brand) as next_ctid
+	from auto
+)
+select
+	prev_ctid - ctid as prev_graps,
+	next_ctid - ctid as next_graps,
+	*
+from ctid_auto;
+
+--- написать запрос, который добавит столбец с позицей каждой модели автомобиля на основе его цены в бренде
+--- модель с самой высокой ценой получает позицию 1
+
+select
+	brand,
+	model,
+	price,
+	rank() over (partition by brand order by price desc) as price_rank
+from
+	auto;
+
+--- написать запрос, который выведет самый дорогой автомобиль в окне модель
+with ranked_autos as(
+	select
+		brand,
+		model,
+		price,
+		rank() over (partition by model order by price desc) as price_rank
+	from
+		auto
+	order by brand, model, price_rank
+)
+select
+	*
+from ranked_autos
+where price_rank = 1;
+
+SELECT TO_DATE('The date is 25-10-2023', '"The date is: "DD-MM-YYYY');
+SELECT TO_DATE('25-10-2023', 'DD-MM-YYYY');
+SELECT TO_DATE('10032025', 'DDMMYYYY'); --- Результат: 2025-03-10
+SELECT TO_DATE(SPLIT_PART('2052-03-10,2025-03-15,2025-03-20', ',', 1), 'YYYY-MM-DD');
+SELECT TO_DATE('10th March 2025', 'DDth Month YYYY'); -- Результат: 2023-10-25
+
+SELECT TO_DATE(SUBSTRING('Дата заказа: 10/03/2025' FROM '\d{2}/\d{2}/\d{4}'), 'DD/MM/YYYY');
+SELECT TO_DATE(SUBSTRING('Заказ №2555541 от 10/03/2025' FROM '\d{2}/\d{2}/\d{4}'), 'DD/MM/YYYY');
+SELECT TO_DATE(SUBSTRING('Дата: 10/03/2025 | Заказ №2555541' FROM '\d{2}/\d{2}/\d{4}'), 'DD/MM/YYYY');
+
+SELECT TO_DATE('Заказ от 10-03-2025', '"Заказ от "DD-MM-YYYY'); -- Результат: 2025-03-10
+
+SELECT TO_TIMESTAMP('2025-03-10 14:30:00', 'YYYY-MM-DD HH24:MI:SS');
+SELECT TO_TIMESTAMP('10/03/2025 02:30 PM', 'DD/MM/YYYY HH12:MI AM');
+
+SELECT '2025-03-10 14:30:00 UTC'::timestamptz;
+SELECT '2025-03-10 14:30:00 UTC'::timestamptz; -- Результат: 2025-03-10 17:30:00.000 +0300
+SELECT TO_TIMESTAMP('10-Mar-2025 14:30:00', 'DD-Mon-YYYY HH24:MI:SS'); -- Результат: 2025-03-10 14:30:00.000
+
+SHOW TIMEZONE;
+
+SELECT TO_TIMESTAMP('1741606200'); -- Результат: 2025-03-10 14:30:00.000
+SELECT '03/10/2025 14:30:00 +03'::timestamptz; -- Результат: 2025-03-10 14:30:00.000 +0300
+
+SELECT EXTRACT(DAY FROM 
+	(TO_DATE(SPLIT_PART('2025-03-10,2025-03-15,2025-03-20', ',', 1), 'YYYY-MM-DD'))
+);
+SELECT DATE_PART('hour', NOW()) AS hour;
+
+SELECT EXTRACT(MONTH FROM NOW()) AS month;
+-- Результат: 10
+
+SELECT EXTRACT(DAY FROM NOW()) AS day;
+-- Результат: 25
+
+SELECT DATE_PART('hour', NOW()) AS hour;
+-- Результат: 14
+
+SELECT TO_CHAR(
+	TO_TIMESTAMP('1741606200'),
+	'DD Mon YY'
+);
+
+SELECT 'FY' || TO_CHAR(TO_TIMESTAMP('1741606200'), 'YY');
+
+SELECT TO_CHAR(
+	TO_TIMESTAMP('1741606200'), -- приводим Unix-время к типу TIMESTAMP 
+	'Mon' 
+);
+
+SELECT TO_CHAR(
+	TO_TIMESTAMP('1741606200'), -- приводим Unix-время к типу TIMESTAMP 
+	'Mon' 
+);
+
+SELECT( 
+	'W' || 
+	EXTRACT(WEEK FROM  TO_TIMESTAMP('1741606200')) || 
+	'_' || 
+	TO_CHAR(TO_TIMESTAMP('1741606200'), 'YY')
+) as week;
+
+
+
+
 
 
 
